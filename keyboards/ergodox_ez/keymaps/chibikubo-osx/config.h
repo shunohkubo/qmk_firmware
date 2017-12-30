5ee6028bce210a1d455e24c56600cd2c46813e20
大久保俊 <ohkubo@ookuboshunnoMacBook-Pro.local>
2017-12-30 09:39:29 +0900

    Add chibikubo's keymaps

---
+++ keyboards/ergodox_ez/keymaps/chibikubo-osx/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 130
+
+#define PERMISSIVE_HOLD
+
+#undef  LED_BRIGHTNESS_DEFAULT
+#define LED_BRIGHTNESS_DEFAULT (LED_BRIGHTNESS_LO)
+
+#endif
