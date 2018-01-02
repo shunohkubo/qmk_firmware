4a39d1ec9af5d155881c7c6fbe2af571e0c70003
Shun Ohkubo <bigbow0929@gmail.com>
2018-01-02 09:38:24 +0900

    Update keymaps

--- keyboards/ergodox_ez/keymaps/chibikubo-osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/chibikubo-osx/keymap.c
@@ -26,15 +26,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | `~     |  1!  |  2@  |  3#  |  4$  |  5%  | LEFT |           | RIGHT|  6^  |  7&  |  8*  |  9(  |  0)  |   [{   |
+ * | `~     |      |      |      |      |      | LEFT |           | RIGHT|      |      |      |      |      |   [{   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   ]}   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /?  |   _    |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /?  |   -_   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      | LAlt | LGui | KP2  |                                       | KP1  |  BS  |      |      |      |
+ *   |      |      | LAlt | LGui | KP2  |                                       | KP1  |  =+  |      |      |      |
  *   |      |      |      |      | /L1  |                                       | /L1  |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,   KC_1,   KC_2,   KC_3,     KC_4,     KC_5,     KC_LEFT,
+        KC_GRV,   KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_LEFT,
         KC_TAB,   KC_Q,   KC_W,   KC_E,     KC_R,     KC_T,     KC_NO,
         KC_LCTL,  KC_A,   KC_S,   KC_D,     KC_F,     KC_G,
         KC_LSFT,  KC_Z,   KC_X,   KC_C,     KC_V,     KC_B,     KC_NO,
@@ -60,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_SPC,   KC_BSPC,  KC_END,
 
         // right hand
-        KC_RGHT,  KC_6,            KC_7,     KC_8,    KC_9,    KC_0,     KC_LBRC,
+        KC_RGHT,  KC_NO,           KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_LBRC,
         KC_NO,    KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,     KC_RBRC,
                   KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-        KC_NO,    KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  LSFT(KC_MINS),
-                  LT(SYMB,KC_P1),  KC_BSPC,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_MINS,
+                  LT(SYMB,KC_P1),  KC_EQL,   KC_NO,   KC_NO,   KC_NO,
 
         TG(SYMB), CTL_T(KC_ESC),
         KC_PGUP,
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     --------------------------------------------------             --------------------------------------------------
    |        |      |      |      |      |      |      |           |      |      |      |      |      |      |  BS    |
    |-------- ------ ------ ------ ------ -------------|           |------ ------ ------ ------ ------ ------ --------|
-   |        |  1   |  2   |  3   |  4   |  5   |      |           |      |  6   |  7   |  8   |  9   |  0   |  =     |
+   |        |  1!  |  2@  |  3#  |  4$  |  5%  |      |           |      |  6^  |  7&  |  8*  |  9(  |  0)  |  =     |
    |-------- ------ ------ ------ ------ ------|      |           |      |------ ------ ------ ------ ------ --------|
    |        |      |      |      |      |      |------|           |------| LEFT | DOWN |  UP  | DOWN |  -   | Enter  |
    |-------- ------ ------ ------ ------ ------|      |           |      |------ ------ ------ ------ ------ --------|
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           |      |      |       |      |        |
                                     ------|------|------|       |------ -------- ------
                                    |      |      |      |       |      |        |      |
-                                   |      |      |------|       |------|        |      |
+                                   |      |      |------|       |------|        | BS   |
                                    |      |      |      |       |      |        |      |
                                     --------------------         ----------------------
  */
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
        KC_TRNS,  KC_TRNS,
        KC_TRNS,
-       KC_TRNS,  KC_TRNS,  KC_TRNS
+       KC_TRNS,  KC_TRNS,  KC_BSPC
 ),
 /* Keymap 2: Media and mouse keys
  *
