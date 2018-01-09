dac997f39b2729dcda128adc85b688ad9ec5cb05
ohkubo <ohkubo@synchro-food.co.jp>
2018-01-09 21:33:00 +0900

    Update keymap

--- keyboards/ergodox_ez/keymaps/chibikubo-osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/chibikubo-osx/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           |      |      |       |      |        |
                                     ------|------|------|       |------ -------- ------
                                    |      |      |      |       |      |        |      |
-                                   |      |      |------|       |------|        | ESC  |
+                                   |      |      |------|       |------|        |      |
                                    |      |      |      |       |      |        |      |
                                     --------------------         ----------------------
  */
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
        KC_TRNS,  KC_TRNS,
        KC_TRNS,
-       KC_TRNS,  KC_TRNS,  KC_ESC
+       KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  *
